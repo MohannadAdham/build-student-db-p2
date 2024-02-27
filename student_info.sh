b15f07f9088b1d258a4048a45190eac127de9aee
Mohannad Adham <mohannad.adm@gmail.com>
2024-02-27 19:35:27 +0000

    Add a third query

--- student_info.sh
+++ student_info.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 # Info about my computer science students from students database
+
 echo -e "\n~~ My Computer Science Students ~~\n"
 
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
@@ -8,4 +10,4 @@ echo -e "\nFirst name, last name, and GPA of students with a 4.0 GPA:"
 echo "$($PSQL "SELECT first_name, last_name, gpa FROM students WHERE gpa = 4.0")"
 
 echo -e "\nAll course names whose first letter is before 'D' in the alphabet:"
-echo "$($PSQL "SELECT course FROM courses WHERE course < 'D';")"
+echo "$($PSQL "SELECT course FROM courses WHERE course < 'D'")"
